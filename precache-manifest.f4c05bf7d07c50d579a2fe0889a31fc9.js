733b4a78efe8a6921493e228270795211509f3ba
Santheep <Santheepkumar@users.noreply.github.com>
2019-11-06 13:57:36 +0530

    Updates

---
+++ precache-manifest.f4c05bf7d07c50d579a2fe0889a31fc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f4802374c12885dcf416f6ec6697336",
+    "url": "/Robofriends-App/index.html"
+  },
+  {
+    "revision": "10aaa807b921fa1c59a4",
+    "url": "/Robofriends-App/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "5075c8afc79e9e2e8923",
+    "url": "/Robofriends-App/static/css/main.8c190050.chunk.css"
+  },
+  {
+    "revision": "10aaa807b921fa1c59a4",
+    "url": "/Robofriends-App/static/js/2.36d0c395.chunk.js"
+  },
+  {
+    "revision": "5075c8afc79e9e2e8923",
+    "url": "/Robofriends-App/static/js/main.7b38fa35.chunk.js"
+  },
+  {
+    "revision": "03937c9991fe50b1c5e3",
+    "url": "/Robofriends-App/static/js/runtime-main.561bea7d.js"
+  },
+  {
+    "revision": "299eceb88df7a7ede1905a58299044ad",
+    "url": "/Robofriends-App/static/media/Orbitron-Bold.299eceb8.ttf"
+  }
+]);
